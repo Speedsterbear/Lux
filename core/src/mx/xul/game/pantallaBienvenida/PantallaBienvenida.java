4fa80d50614836daab6576099b1b74c44f8e7333
egaon <david.gaona@ideen.mx>
2021-05-25 19:18:37 -0500

    Vitral_Fixed

--- core/src/mx/xul/game/pantallaBienvenida/PantallaBienvenida.java
+++ core/src/mx/xul/game/pantallaBienvenida/PantallaBienvenida.java
@@ -80,8 +80,8 @@ public class PantallaBienvenida extends Pantalla {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        stageLogo.act();
-        stageLogo.draw();
+       stageLogo.act();
+       stageLogo.draw();
     }
 
     @Override
